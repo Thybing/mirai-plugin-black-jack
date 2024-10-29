292b77e64f105305a7b40dbdd03639a446469084
Thybing <yanbinghao86@gmail.com>
2024-10-29 19:42:47 +0800

    为Player增加操作金钱的接口

--- src/main/kotlin/blackjack/Player.kt
+++ src/main/kotlin/blackjack/Player.kt
@@ -3,6 +3,15 @@ package org.example.mirai.plugin.blackjack
 internal abstract class Player(val name : String, val uniqueCode : ULong,  var money : Int) {
     //玩家当前正在操作的手牌
     val curHand : HandCard = HandCard()
+
+    fun changeMoney(delta : Int) : Boolean {
+        return if (money + delta >= 0) {
+            money += delta
+            true
+        }else {
+            false
+        }
+    }
 }
 
 
