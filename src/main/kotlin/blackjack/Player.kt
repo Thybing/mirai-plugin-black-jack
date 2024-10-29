3ad660373c483bcec8c506f03a3102ffb15d2a37
Thybing <yanbinghao86@gmail.com>
2024-10-29 19:14:36 +0800

    完成玩家类Player和两个子类，庄家Banker，闲家Punter

---
+++ src/main/kotlin/blackjack/Player.kt
@@ -0,0 +1,18 @@
+package org.example.mirai.plugin.blackjack
+
+internal abstract class Player(val name : String, val uniqueCode : ULong,  var money : Int) {
+    //玩家当前正在操作的手牌
+    val curHand : HandCard = HandCard()
+}
+
+
+internal class Banker(name : String, uniqueCode : ULong, money: Int) : Player(name, uniqueCode, money) {
+
+}
+
+internal class Punter(name : String, uniqueCode : ULong, money: Int) : Player(name, uniqueCode, money) {
+    val preHand : MutableList<HandCard> = mutableListOf()
+    val splitStack : MutableList<HandCard> = mutableListOf()
+
+    var chip: Int = 0
+}
